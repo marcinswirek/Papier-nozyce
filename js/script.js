b4afc39159afb0fb9b617ff7369fb6e6040da5e7
Marcin Świrek <swirek.marcin@gmail.com>
2018-10-02 23:03:35 +0200

    Change var on let in js file

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 (function() {
   //variable definition
-  var newGameBtn = document.getElementById("js-newGameButton"),
-    pickRock = document.getElementById("js-playerPick_rock"),
-    pickPaper = document.getElementById("js-playerPick_paper"),
-    pickScissors = document.getElementById("js-playerPick_scissors"),
-    playerPointsElem = document.getElementById("js-playerPoints"),
-    playerNameElem = document.getElementById("js-playerName"),
-    computerPointsElem = document.getElementById("js-computerPoints"),
-    playerPickElem = document.getElementById("js-playerPick"),
-    computerPickElem = document.getElementById("js-computerPick"),
-    playerResultElem = document.getElementById("js-playerResult"),
-    computerResultElem = document.getElementById("js-computerResult"),
-    newGameElem = document.getElementById("js-newGameElement"),
-    pickElem = document.getElementById("js-playerPickElement"),
-    resultsElem = document.getElementById("js-resultsTableElement"),
+  let newGameBtn = document.getElementById('js-newGameButton'),
+    pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors'),
+    playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints'),
+    playerPickElem = document.getElementById('js-playerPick'),
+    computerPickElem = document.getElementById('js-computerPick'),
+    playerResultElem = document.getElementById('js-playerResult'),
+    computerResultElem = document.getElementById('js-computerResult'),
+    newGameElem = document.getElementById('js-newGameElement'),
+    pickElem = document.getElementById('js-playerPickElement'),
+    resultsElem = document.getElementById('js-resultsTableElement'),
     player = {
-      name: "",
+      name: '',
       score: 0
     },
     computer = {
@@ -23,45 +23,45 @@
     };
 
   //bind UI events
-  newGameBtn.addEventListener("click", newGame);
+  newGameBtn.addEventListener('click', newGame);
 
-  pickRock.addEventListener("click", function() {
-    playerPick("rock");
+  pickRock.addEventListener('click', function() {
+    playerPick('rock');
   });
 
-  pickPaper.addEventListener("click", function() {
-    playerPick("paper");
+  pickPaper.addEventListener('click', function() {
+    playerPick('paper');
   });
 
-  pickScissors.addEventListener("click", function() {
-    playerPick("scissors");
+  pickScissors.addEventListener('click', function() {
+    playerPick('scissors');
   });
 
   //start game
-  setGameElements("notStarted");
+  setGameElements('notStarted');
 
   function setGameElements(gameState) {
     switch (gameState) {
-      case "started":
-        newGameElem.style.display = "none";
-        pickElem.style.display = "block";
-        resultsElem.style.display = "block";
+      case 'started':
+        newGameElem.style.display = 'none';
+        pickElem.style.display = 'block';
+        resultsElem.style.display = 'block';
         break;
-      case "ended":
-        newGameBtn.innerText = "Jeszcze raz";
-      case "notStarted":
+      case 'ended':
+        newGameBtn.innerText = 'Jeszcze raz';
+      case 'notStarted':
       default:
-        newGameElem.style.display = "block";
-        pickElem.style.display = "none";
-        resultsElem.style.display = "none";
+        newGameElem.style.display = 'block';
+        pickElem.style.display = 'none';
+        resultsElem.style.display = 'none';
     }
   }
 
   function newGame() {
-    player.name = prompt("Please enter your name", "imie gracza");
+    player.name = prompt('Please enter your name', 'imie gracza');
     if (player.name) {
       player.score = computer.score = 0;
-      setGameElements("started");
+      setGameElements('started');
 
       playerNameElem.innerHTML = player.name;
       setGamePoints();
@@ -69,12 +69,12 @@
   }
 
   function getComputerPick() {
-    var possiblePicks = ["rock", "paper", "scissors"];
+    let possiblePicks = ['rock', 'paper', 'scissors'];
     return possiblePicks[Math.floor(Math.random() * 3)];
   }
 
   function playerPick(playerPick) {
-    var computerPick = getComputerPick();
+    let computerPick = getComputerPick();
 
     playerPickElem.innerHTML = playerPick;
     computerPickElem.innerHTML = computerPick;
@@ -83,25 +83,25 @@
   }
 
   function checkRoundWinner(playerPick, computerPick) {
-    playerResultElem.innerHTML = computerResultElem.innerHTML = "";
+    playerResultElem.innerHTML = computerResultElem.innerHTML = '';
 
-    var winnerIs = "player";
+    let winnerIs = 'player';
 
     if (playerPick === computerPick) {
-      winnerIs = "noone"; // remis
+      winnerIs = 'noone'; // remis
     } else if (
-      (computerPick === "rock" && playerPick === "scissors") ||
-      (computerPick === "scissors" && playerPick === "paper") ||
-      (computerPick === "paper" && playerPick === "rock")
+      (computerPick === 'rock' && playerPick === 'scissors') ||
+      (computerPick === 'scissors' && playerPick === 'paper') ||
+      (computerPick === 'paper' && playerPick === 'rock')
     ) {
-      winnerIs = "computer";
+      winnerIs = 'computer';
     }
 
-    if (winnerIs === "player") {
-      playerResultElem.innerHTML = "Win!";
+    if (winnerIs === 'player') {
+      playerResultElem.innerHTML = 'Win!';
       player.score++;
-    } else if (winnerIs === "computer") {
-      computerResultElem.innerHTML = "Win!";
+    } else if (winnerIs === 'computer') {
+      computerResultElem.innerHTML = 'Win!';
       computer.score++;
     }
     setGamePoints();
@@ -115,13 +115,13 @@
 
   function endGame() {
     if (computer.score === 10) {
-      alert("Wygrał komputer");
-      setGameElements("ended");
+      alert('Wygrał komputer');
+      setGameElements('ended');
     }
 
     if (player.score === 10) {
-      alert("Wygrał " + player.name);
-      setGameElements("ended");
+      alert('Wygrał ' + player.name);
+      setGameElements('ended');
     }
   }
 })();
